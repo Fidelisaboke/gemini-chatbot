5e508c8d966acb6e830b7546500addcf17840cf8
Fidel Isaboke <fidelisaboke@gmail.com>
2025-01-05 12:23:35 +0300

    Add avatars, loading animation and typing effect

--- client/app.js
+++ client/app.js
@@ -3,19 +3,76 @@ endpoint and display the response on the webpage. */
 
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
+// Custom renderer for marked.js
+const renderer = new marked.Renderer();
+renderer.paragraph = (text) => {
+  return `<p class="text">${text}</p>`;
+};
+
 let promptForm = document.getElementById("prompt-form");
-let results = document.getElementById("results");
 
 promptForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  let prompt = document.getElementById("prompt").value;
-
-  processPrompt(prompt);
+  handleOutgoingMessage();
 });
 
 /**
+ *  Handle the outgoing message entered by the user.
+ */
+function handleOutgoingMessage() {
+  let prompt = document.getElementById("prompt").value.trim();
+  let chatList = document.querySelector(".chat-list");
+  if (!prompt) return;
+  let html = `
+      <div class="message-content">
+        <img src="images/user.svg" alt="User Avatar" class="avatar" />
+          <p class="text">${prompt}</p>
+      </div>
+  `;
+  const outgoingMessageDiv = createMessageElement(html, "outgoing");
+  chatList.appendChild(outgoingMessageDiv);
+  promptForm.reset();
+
+  setTimeout(showLoadingAnimation, 500);
+  processPrompt(prompt);
+}
+
+/**
+ * Create a message element and append it to the chat window.
+ * @param {*} message - The message to be displayed
+ * @param {*} type - The type of message (incoming or outgoing)
+ * @param  {...any} classes - Additional classes to be added to the message element
+ */
+function createMessageElement(html, ...classes) {
+  let messageDiv = document.createElement("div");
+  messageDiv.classList.add("message", ...classes);
+  messageDiv.innerHTML = html;
+  return messageDiv;
+}
+
+/**
+ * Show a loading animation while the response is being fetched.
+ */
+function showLoadingAnimation() {
+  let chatList = document.querySelector(".chat-list");
+  let html = `
+      <div class="message-content">
+        <img src="images/gemini.svg" alt="Gemini Avatar" class="avatar" />
+          <p class="text"></p>
+          <div class="loading-indicator">
+            <div class="loading-bar"></div>
+            <div class="loading-bar"></div>
+            <div class="loading-bar"></div>
+          </div>
+      </div>
+  `;
+  const loadingMessageDiv = createMessageElement(html, "incoming", "loading");
+  chatList.appendChild(loadingMessageDiv);
+}
+
+/**
  * Process the prompt entered by the user.
- * @param {*} prompt
+ * @param {*} prompt - The prompt entered by the user
  */
 function processPrompt(prompt) {
   // Get the API key from the server
@@ -33,7 +90,7 @@ function processPrompt(prompt) {
       const modelName = "gemini-1.5-flash:generateContent";
       const API_URL = `${baseUrl}${modelName}?key=${API_KEY}`;
 
-      getPromptResults(prompt, API_URL);
+      handlePromptResults(prompt, API_URL);
     })
     .catch((error) => {
       console.error(error);
@@ -42,10 +99,10 @@ function processPrompt(prompt) {
 
 /**
  * Get the response from the GPT-3 API
- * @param {*} prompt
- * @param {*} API_URL
+ * @param {*} prompt - The prompt entered by the user
+ * @param {*} API_URL - The URL of the GPT-3 API
  */
-function getPromptResults(prompt, API_URL) {
+function handlePromptResults(prompt, API_URL) {
   const data = {
     contents: [
       {
@@ -66,15 +123,55 @@ function getPromptResults(prompt, API_URL) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      let responses = data.candidates[0].content.parts;
-      responses.forEach((response) => {
-        let responseText = marked.parse(response.text);
-        results.innerHTML += responseText;
-        console.log(response.text);
-      });
+      const apiResponse = data?.candidates[0].content.parts[0].text;
+      const textElement = handleIncomingMessage(apiResponse);
+      showTypingEffect(apiResponse, textElement);
     })
     .catch((error) => {
-      results.innerHTML = error;
       console.log(error);
+    })
+    .finally(() => {
+      let loadingMessage = document.querySelector(".loading");
+      loadingMessage.remove();
     });
+}
+
+/**
+ * Display the response from the GPT-3 API
+ * @param {*} response - The response from the GPT-3 API
+ */
+function handleIncomingMessage(response) {
+  let chatList = document.querySelector(".chat-list");
+  let html = `
+      <div class="message-content">
+        <img src="images/gemini.svg" alt="Gemini Avatar" class="avatar" />
+        <p class="text">${response}</p>
+      </div>
+      <span class="icon material-symbols-rounded">content_copy</span>
+  `;
+  const incomingMessageDiv = createMessageElement(html, "incoming");
+  chatList.appendChild(incomingMessageDiv);
+
+  const textElement = incomingMessageDiv.querySelector(".text");
+  return textElement;
+}
+
+/**
+ * Show typing effect
+ * @param {*} response - The response from the GPT-3 API
+ * @param {*} textElement - The text element to display the response
+ */
+function showTypingEffect(response, textElement) {
+  const words = response.split(" ");
+  const typingSpeed = 100;
+  let i = 0;
+
+  const interval = setInterval(() => {
+    if (i === words.length) {
+      clearInterval(interval);
+      return;
+    }
+    textElement.textContent += " " + words[i];
+    i++;
+  }, typingSpeed);
 }
